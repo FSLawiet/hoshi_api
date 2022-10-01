84da6c186126e7f98246592b771c4bfee6605d00
Fausto André <fslawliet@gmail.com>
2022-10-01 14:25:56 -0300

    Yeet

--- src/infra/connection.js
+++ src/infra/connection.js
@@ -2,7 +2,7 @@ const pgp = require("pg-promise")();
 const bancoDeDados = pgp({
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
-  host: "dpg-ccrifqirrk08u0m08k60-a.oregon-postgres.render.com",
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
   ssl: true,
