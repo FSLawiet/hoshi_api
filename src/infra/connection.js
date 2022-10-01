b8628d6c66dc76bf188bf036a4f4ae50c6b29c13
Fausto André <fslawliet@gmail.com>
2022-10-01 17:47:21 -0300

    Yeeeeeeeeeeeeet

--- src/infra/connection.js
+++ src/infra/connection.js
@@ -1,6 +1,6 @@
 const pgp = require("pg-promise")();
 const bancoDeDados = pgp({
-  connectionString: process.env.DB_CONN_STRING,
+  connectionString: process.env.DB_CONN_STRING.toString(),
   max: 30,
 });
 
